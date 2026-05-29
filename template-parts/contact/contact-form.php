f46d6238b4701e542b2245b9694433dbb6b6ab00
Chris Brosky <seebrosky@gmail.com>
2026-05-29 10:43:09 -0700

    Adjusted padding on contact page

--- template-parts/contact/contact-form.php
+++ template-parts/contact/contact-form.php
@@ -11,7 +11,7 @@
 <section class="bg-site-bg py-10">
     <div class="mx-auto grid max-w-6xl gap-8 px-6 lg:grid-cols-[1.35fr_0.95fr]">
 
-        <div class="rounded-2xl border border-border-light bg-white p-8 lg:p-10">
+        <div class="rounded-xl border border-border-light bg-white p-8 pb-5 lg:pb-5">
             <h2 class="mb-3 text-3xl font-extrabold tracking-[-0.04em] text-site-text">
                 Send Us A Message
             </h2>
@@ -23,7 +23,7 @@
             <?php echo do_shortcode('[fluentform id="3"]'); ?>
         </div>
 
-        <aside class="rounded-2xl border border-border-light bg-white p-8 lg:p-10">
+        <aside class="rounded-xl border border-border-light bg-white p-8 pb-9 lg:pb-5">
             <h2 class="mb-3 text-3xl font-extrabold tracking-[-0.04em] text-site-text">
                 Contact Information
             </h2>
@@ -35,7 +35,7 @@
             <div class="space-y-8">
                 <div class="flex gap-5">
                     <div class="flex size-12 shrink-0 items-center justify-center rounded-xl bg-emerald-600/10 text-emerald-600">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-7">
                             <path fill-rule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clip-rule="evenodd" />
                         </svg>
                     </div>
