7294bb31575228ab0f0b2e7a3c989306babdfda3
Chris Brosky <seebrosky@gmail.com>
2026-06-19 08:51:47 -0700

    Adjusted page.php padding

--- page.php
+++ page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main class="mx-auto max-w-4xl px-6 py-16">
+<main class="mx-auto max-w-6xl px-6 py-16">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <article>
             <h1 class="text-4xl font-bold"><?php the_title(); ?></h1>
