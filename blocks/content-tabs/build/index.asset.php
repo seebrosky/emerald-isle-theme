b8fd2af588b9967f31100debd397f61a2e24dba3
Chris Brosky <seebrosky@gmail.com>
2026-06-21 12:36:19 -0700

    Bust Content Tabs frontend script cache

--- blocks/content-tabs/build/index.asset.php
+++ blocks/content-tabs/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '348a8d5f07393fff0b7b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '77cc41290fa3593b43a4');
